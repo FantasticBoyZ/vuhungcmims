47ed37e16154d457fe391ef15311d842d621d0b1
FantasticBoyZ <khoidragonslayer@gmail.com>
2022-05-22 23:01:11 +0700

    add authentication JWT

---
+++ src/components/BoardUser.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from "react";
+import UserService from "../services/user.service";
+const BoardUser = () => {
+  const [content, setContent] = useState([]);
+  useEffect(() => {
+    UserService.getUserBoard().then(
+      (response) => {
+        setContent(response.data);
+        console.log(response.data);
+      },
+      (error) => {
+        const _content =
+          (error.response &&
+            error.response.data &&
+            error.response.data.message) ||
+          error.message ||
+          error.toString();
+        setContent(_content);
+      }
+    );
+  }, []);
+  return (
+    <div className="container">
+      <header className="jumbotron">
+        {content.map((person) => (
+          <h3 key={person.id} >
+            {person.id}.{person.name}{" "}
+          </h3>
+        ))}
+      </header>
+    </div>
+  );
+};
+export default BoardUser;
