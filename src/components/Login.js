ea486dc1493913bf5f4a62180acc5a8419e384d8
Nguyen_Hien <nguyenhienyc123@gmail.com>
2022-06-02 08:48:58 -0700

    Login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,29 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
-import Form from "react-validation/build/form";
-import Input from "react-validation/build/input";
-import CheckButton from "react-validation/build/button";
-import AuthService from "@/services/auth.service";
+import Form from 'react-validation/build/form';
+import Input from 'react-validation/build/input';
+import CheckButton from 'react-validation/build/button';
+import AuthService from '@/services/auth.service';
+import LayoutLogin from '@/pages/Login/Login';
+import Checkbox from '@mui/material/Checkbox';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Link from '@mui/material/Link';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import '../pages/Login/Login.css';
+
 const required = (value) => {
   if (!value) {
     return (
-      <div className="alert alert-danger" role="alert">
+      <div
+        className="alert alert-danger"
+        role="alert"
+      >
         This field is required!
       </div>
     );
@@ -17,10 +33,10 @@ const Login = () => {
   let navigate = useNavigate();
   const form = useRef();
   const checkBtn = useRef();
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
   const [loading, setLoading] = useState(false);
-  const [message, setMessage] = useState("");
+  const [message, setMessage] = useState('');
   const onChangeUsername = (e) => {
     const username = e.target.value;
     setUsername(username);
@@ -31,81 +47,172 @@ const Login = () => {
   };
   const handleLogin = (e) => {
     e.preventDefault();
-    setMessage("");
+    setMessage('');
     setLoading(true);
     form.current.validateAll();
     if (checkBtn.current.context._errors.length === 0) {
       AuthService.login(username, password).then(
         () => {
-          navigate("/profile");
+          navigate('/profile');
           window.location.reload();
         },
         (error) => {
           const resMessage =
-            (error.response &&
-              error.response.data &&
-              error.response.data.message) ||
+            (error.response && error.response.data && error.response.data.message) ||
             error.message ||
             error.toString();
           setLoading(false);
           setMessage(resMessage);
-        }
+        },
       );
     } else {
       setLoading(false);
     }
   };
   return (
-    <div className="col-md-12">
-      Login
-      {/* <div className="card card-container">
-        <img
-          src="//ssl.gstatic.com/accounts/ui/avatar_2x.png"
-          alt="profile-img"
-          className="profile-img-card"
-        />
-        <Form onSubmit={handleLogin} ref={form}>
-          <div className="form-group">
-            <label htmlFor="username">Username</label>
-            <Input
-              type="text"
-              className="form-control"
-              name="username"
+    <LayoutLogin>
+      <Grid
+        item
+        component="main"
+        sx={{ height: '100vh' }}
+      >
+        <Box className="box-login">
+          <Avatar
+            className="logo"
+            variant="square"
+          >
+            LOGO
+          </Avatar>
+          <Typography
+            component="h1"
+            variant="h5"
+          >
+            Sign in
+          </Typography>
+
+          <Box
+            component="form"
+            noValidate
+            onSubmit={handleLogin}
+            sx={{ mt: 1 }}
+          >
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email"
+              name="email"
+              autoComplete="email"
+              autoFocus
               value={username}
               onChange={onChangeUsername}
               validations={[required]}
             />
-          </div>
-          <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <Input
-              type="password"
-              className="form-control"
+            <TextField
+              margin="normal"
+              required
+              fullWidth
               name="password"
-              value={password}
-              onChange={onChangePassword}
+              label="Mật khẩu"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              value={username}
+              onChange={onChangeUsername}
               validations={[required]}
             />
-          </div>
-          <div className="form-group">
-            <button className="btn btn-primary btn-block" disabled={loading}>
-              {loading && (
-                <span className="spinner-border spinner-border-sm"></span>
-              )}
-              <span>Login</span>
-            </button>
-          </div>
-          {message && (
+            <FormControlLabel
+              control={
+                <Checkbox
+                  value="remember"
+                  color="primary"
+                />
+              }
+              label="Lưu đăng nhập"
+            />
+            <Button
+              className="btn-submit"
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Đăng nhập
+            </Button>
+            <Grid container>
+              <Grid
+                item
+                xs
+              >
+                <Link
+                  href="#"
+                  variant="body2"
+                >
+                  Quên mật khẩu?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link
+                  href="#"
+                  variant="body2"
+                >
+                  {'Bạn chưa có tài khoản? Đăng ký'}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Grid>
+
+      {/* <div className="card card-container">
+          <img
+            src="//ssl.gstatic.com/accounts/ui/avatar_2x.png"
+            alt="profile-img"
+            className="profile-img-card"
+          />
+          <Form onSubmit={handleLogin} ref={form}>
             <div className="form-group">
-              <div className="alert alert-danger" role="alert">
-                {message}
-              </div>
+              <label htmlFor="username">Username</label>
+              <Input
+                type="text"
+                className="form-control"
+                name="username"
+                value={username}
+                onChange={onChangeUsername}
+                validations={[required]}
+              />
             </div>
-          )}
-          <CheckButton style={{ display: "none" }} ref={checkBtn} />
-        </Form>
-      </div> */}
-    </div>
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <Input
+                type="password"
+                className="form-control"
+                name="password"
+                value={password}
+                onChange={onChangePassword}
+                validations={[required]}
+              />
+            </div>
+            <div className="form-group">
+              <button className="btn btn-primary btn-block" disabled={loading}>
+                {loading && (
+                  <span className="spinner-border spinner-border-sm"></span>
+                )}
+                <span>Login</span>
+              </button>
+            </div>
+            {message && (
+              <div className="form-group">
+                <div className="alert alert-danger" role="alert">
+                  {message}
+                </div>
+              </div>
+            )}
+            <CheckButton style={{ display: "none" }} ref={checkBtn} />
+          </Form>
+        </div> */}
+    </LayoutLogin>
   );
 };
 export default Login;
---
+++ src/pages/Login/Login.js
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+import './Login.css';
+
+import Carousel from 'react-material-ui-carousel';
+
+const LayoutLogin = ({ children }) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const items = [
+    {
+      src: 'src/assets/images/image1.png',
+      altText: 'Slide 1',
+    },
+    {
+      src: 'src/assets/images/image2.png',
+      altText: 'Slide 2',
+    },
+    {
+      src: 'src/assets/images/image3.png',
+      altText: 'Slide 3',
+    },
+  ];
+
+  const handleChange = (cur, prev) => {
+    setActiveIndex(cur);
+  };
+
+  // const url = items[activeIndex].src
+  const slides = items.map((item, i) => {
+    return (
+      <img
+        key={i}
+        src={require('src/assets/images/image3.png')}
+        alt={item.altText}
+      />
+    );
+  });
+  return (
+    <>
+      <main>
+        <div className="flexContainer">
+          <div className="flexItemLeft  my__carousel_main">
+            <Carousel
+              index={activeIndex}
+              onChange={handleChange}
+              interval={8000}
+              animation="slide"
+              stopAutoPlayOnHover
+              indicators={true}
+              swipe
+              className="my-carousel"
+            >
+              {slides}
+              {/* {items.map((item, i) => {
+                <img
+                  key={i}
+                  // src={require(item.src)}
+                  src="src/assets/images/image3.png"
+                  alt={item.altText}
+                />;
+              })} */}
+            </Carousel>
+          </div>
+          <div className="flexItemRight">{children}</div>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default LayoutLogin;
