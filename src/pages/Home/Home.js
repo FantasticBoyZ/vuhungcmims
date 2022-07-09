bb3cfc9d1fd60f9f1355efc11a3dc2a501bd2810
FantasticBoyZ <khoidragonslayer@gmail.com>
2022-07-09 11:59:46 +0700

    add new design homepage

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,71 @@
 import { removePost } from '@/slices/PostSlice';
 import testAPI from '@/utils/testApi';
-import { Box, Button, Card, Container, Paper, Typography } from '@mui/material';
+import { AccountCircle, Search } from '@mui/icons-material';
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  CardHeader,
+  Container,
+  Grid,
+  InputAdornment,
+  Paper,
+  TextField,
+  Typography,
+} from '@mui/material';
+import { makeStyles } from '@mui/styles';
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
+import InventoryChart from '@/pages/Home/InventoryChart';
 
+const useStyles = makeStyles((theme) => ({
+  cardInfo: {
+    minHeight: '180px',
+  },
+  contentContainer: {
+    minHeight: '180px',
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },
+  widget: {
+    flexGrow: 1,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: '24px',
+    color: '#9FA2B4',
+    textAlign: 'center',
+    fontWeight: '500',
+    margin: '0',
+  },
+  number: {
+    fontSize: '40px',
+    color: '#252733',
+    fontWeight: '500',
+    margin: '0',
+  },
+  searchField: {
+    minHeight: '53px',
+    backgroundColor: '#FFFFFF',
+    borderRadius: '50px',
+    outline: 'none',
+  },
+  noBorder: {
+    border: 'none',
+  },
+  chart: {
+    height: '50vh',
+    width: '100%'
+  }
+}));
 const HomePage = () => {
+  const classes = useStyles();
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const [content, setContent] = useState([]);
@@ -43,8 +103,109 @@ const HomePage = () => {
   // }, []);
   return (
     <Box>
-      <Container maxWidth='xl'>
-        {/* <Card>HomePage</Card> */}
+      <Container maxWidth="xl">
+        <Grid
+          container
+          spacing={3}
+        >
+          <Grid
+            xs={3}
+            item
+          >
+            <Card className={classes.cardInfo}>
+              <CardContent className={classes.contentContainer}>
+                <Box className={classes.widget}>
+                  <p className={classes.title}>Số nhân viên hiện tại</p>
+                </Box>
+                <Box className={classes.widget}>
+                  <p className={classes.number}>6</p>
+                </Box>
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid
+            xs={3}
+            item
+          >
+            <Card className={classes.cardInfo}>
+              <CardContent className={classes.contentContainer}>
+                <Box className={classes.widget}>
+                  <p className={classes.title}>Số đơn nhập đang chờ xét duyệt</p>
+                </Box>
+                <Box className={classes.widget}>
+                  <p className={classes.number}>6</p>
+                </Box>
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid
+            xs={3}
+            item
+          >
+            <Card className={classes.cardInfo}>
+              <CardContent className={classes.contentContainer}>
+                <Box className={classes.widget}>
+                  <p className={classes.title}>Số đơn xuất đang chờ xét duyệt</p>
+                </Box>
+                <Box className={classes.widget}>
+                  <p className={classes.number}>6</p>
+                </Box>
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid
+            xs={3}
+            item
+          >
+            <Card className={classes.cardInfo}>
+              <CardContent className={classes.contentContainer}>
+                <Box className={classes.widget}>
+                  <p className={classes.title}>Số đơn hàng lưu kho</p>
+                </Box>
+                <Box className={classes.widget}>
+                  <p className={classes.number}>6</p>
+                </Box>
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid
+            xs={12}
+            item
+          >
+            <TextField
+              id="outlined-basic"
+              className={classes.searchField}
+              name="functionSearch"
+              placeholder="Tìm kiếm chức năng..."
+              fullWidth
+              label={null}
+              variant="outlined"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <Search />
+                  </InputAdornment>
+                ),
+              }}
+              // onKeyDown={handleSearch}
+              // onChange={handleSearchChange}
+            />
+          </Grid>
+          <Grid
+            xs={12}
+            item
+          >
+            <Card>
+              <CardHeader title="Lượng tồn kho theo tháng" />
+              <CardContent>
+                <Box className={classes.chart}>
+                  <InventoryChart />
+                </Box>
+              </CardContent>
+            </Card>
+          </Grid>
+        </Grid>
+        <InventoryChart />
       </Container>
       {/* <Button
         variant="contained"
