aa8f4b4424ff2c2ad0624ae0cd06156bb9a88dd2
FantasticBoyZ <khoidragonslayer@gmail.com>
2022-07-30 01:03:58 +0700

    change api url

--- src/constants/apiUrl.js
+++ src/constants/apiUrl.js
@@ -1,5 +1,5 @@
-// export const API_URL = 'http://localhost:8080/api'
-// export const API_URL_IMAGE = 'http://localhost:8080'
+export const API_URL = 'http://localhost:8080/api'
+export const API_URL_IMAGE = 'http://localhost:8080'
 
-export const API_URL = process.env.REACT_APP_API_URL
-export const API_URL_IMAGE = 'http://ec2-52-221-240-240.ap-southeast-1.compute.amazonaws.com:8080'
+// export const API_URL = process.env.REACT_APP_API_URL
+// export const API_URL_IMAGE = 'http://ec2-52-221-240-240.ap-southeast-1.compute.amazonaws.com:8080'
