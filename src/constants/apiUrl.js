842929e7e14620439c6a74a4d1b4a135d0e9b688
NinhTBMHE141325 <ninhtbmhe141325@fpt.edu.vn>
2022-08-16 00:23:57 +0700

    add UAT deployment configure files

--- src/constants/apiUrl.js
+++ src/constants/apiUrl.js
@@ -2,4 +2,4 @@
 // export const API_URL_IMAGE = 'http://localhost:8080'
 
 export const API_URL = process.env.REACT_APP_API_URL
-export const API_URL_IMAGE = 'http://ec2-18-140-70-87.ap-southeast-1.compute.amazonaws.com:8080'
+export const API_URL_IMAGE = 'http://ec2-13-127-64-212.ap-south-1.compute.amazonaws.com:8080'
